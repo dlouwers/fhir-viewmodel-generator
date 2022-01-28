d9328736ce2c49448e9b5befde0de46d1c1bbe93
Dirk Louwers <dirk.louwers@stormlantern.nl>
2022-01-28 14:07:47 +0100

    FIrst attempt for syntax receiver

--- FhirViewModelGeneratorTests/ExampleTests.cs
+++ FhirViewModelGeneratorTests/ExampleTests.cs
@@ -1,12 +1,12 @@
-﻿using Hl7.Fhir.Model;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using Xunit;
 
 namespace FhirViewModelGeneratorTests
@@ -18,10 +18,10 @@ namespace FhirViewModelGeneratorTests
         {
             var patient = CreateResourceFromJson<Patient>("Resources/01-Patient.json");
 
-            var jsonText = JsonConvert.SerializeObject(patient, Formatting.Indented,  new JsonSerializerSettings
+            var jsonText = JsonConvert.SerializeObject(patient, Formatting.Indented, new JsonSerializerSettings
             {
                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                Converters = new List<JsonConverter>() { new StringEnumConverter() },
+                Converters = new List<JsonConverter> { new StringEnumConverter() },
                 NullValueHandling = NullValueHandling.Ignore,
                 StringEscapeHandling = StringEscapeHandling.EscapeNonAscii
             });
@@ -32,14 +32,13 @@ namespace FhirViewModelGeneratorTests
         // Rider creates such UTF-8 files without BOM. Visual Studio creates UTF-16LE text files with little endian byte mark by default. 
         private static T CreateResourceFromJson<T>(string filePath) where T : Resource, new()
         {
-            T resource = new T();
+            var resource = new T();
 
-            if (!File.Exists(filePath))
-            { throw new Exception($"Resource file {filePath} does not exist"); }
+            if (!File.Exists(filePath)) throw new Exception($"Resource file {filePath} does not exist");
 
-            using (var r = new StreamReader(filePath, Encoding.UTF8, detectEncodingFromByteOrderMarks: false))
+            using (var r = new StreamReader(filePath, Encoding.UTF8, false))
             {
-                string json = r.ReadToEnd();
+                var json = r.ReadToEnd();
                 var fhirJsonParser = new FhirJsonParser();
                 resource = fhirJsonParser.Parse<T>(json);
             }
